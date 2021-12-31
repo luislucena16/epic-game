a4bb85efad97585d494c3bae5bce8c7f5d08077a
Luis Lucena <llucena@shokworks.io>
2021-12-30 23:58:57 -0400

    mint NFT and Base64 config

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.0;
 // NFT contract to inherit from.
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+// Helper we wrote to encode in Base64
+import "../libraries/Base64.sol";
+
 // Helper functions OpenZeppelin provides.
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
@@ -94,5 +97,32 @@ contract MyEpicGame is ERC721 {
 
     // Increment the tokenId for the next person that uses it.
     _tokenIds.increment();
+  }
+
+    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+  CharacterAttributes memory charAttributes = nftHolderAttributes[_tokenId];
+
+  string memory strHp = Strings.toString(charAttributes.hp);
+  string memory strMaxHp = Strings.toString(charAttributes.maxHp);
+  string memory strAttackDamage = Strings.toString(charAttributes.attackDamage);
+
+  string memory json = Base64.encode(
+    abi.encodePacked(
+      '{"name": "',
+      charAttributes.name,
+      ' -- NFT #: ',
+      Strings.toString(_tokenId),
+      '", "description": "This is an NFT that lets people play in the game Metaverse Slayer!", "image": "',
+      charAttributes.imageURI,
+      '", "attributes": [ { "trait_type": "Health Points", "value": ',strHp,', "max_value":',strMaxHp,'}, { "trait_type": "Attack Damage", "value": ',
+      strAttackDamage,'} ]}'
+    )
+  );
+
+  string memory output = string(
+    abi.encodePacked("data:application/json;base64,", json)
+  );
+  
+  return output;
   }
 }
